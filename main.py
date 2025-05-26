84e1f57d3cec9694aa1412687926ce1bcdc05059
Pappalaity <pappalaity@gmail.com>
2025-05-26 12:28:30 +0000

    refactor model to models

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+* Main Function
+
+"""
+
+from models.Kmean import KMeans
+from models.Kmean_plus import KMeans_plus
+
+
+if __name__ == "__main__":
+    kmeans = KMeans()
+    kmeans_plus = KMeans_plus()
