69165d65d1334150520799e4438aeb635f563331
Pappalaity <pappalaity@gmail.com>
2025-05-26 12:33:15 +0000

    load datasets and show head in main file

--- main.py
+++ main.py
@@ -3,10 +3,17 @@
 
 """
 
+import pandas as pd
 from models.Kmean import KMeans
 from models.Kmean_plus import KMeans_plus
 
 
 if __name__ == "__main__":
-    kmeans = KMeans()
-    kmeans_plus = KMeans_plus()
+    # kmeans = KMeans()
+    # kmeans_plus = KMeans_plus()
+
+    # Load Datasets 
+
+    print("Hello From Main")
+    datasets = pd.read_csv('datasets/Mall_Customers.csv')
+    print(datasets.head())
